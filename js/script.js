64485057c601bd09b64b8345dfdf48539541c83f
Jake <Jakeacole@gmail.com>
2015-06-22 01:44:20 -0400

    Converted site to bootstrap

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-//Script file
+//Script file that allows slow scroll for nav bar links
 
 $(document).ready(function(){
+
+	//Variables link to each nav link id
 	var contact = $('#nav1');
 	var education = $("#nav2");
 	var experience = $('#nav3');
 	var qualifications = $('#nav4');
 
+	//Slow scrolls to the contact section
 	contact.click(function() {
-	  $("html, body").animate({ scrollTop: $('#contact-and-pic').offset().top }, "slow")
-	  //return false;
-	});
-
-	education.click(function() {
-	  $("html, body").animate({ scrollTop: $('#education').offset().top }, "slow")
-	  //return false;
-	});
-
-	experience.click(function() {
-	  $("html, body").animate({ scrollTop: $('#experience').offset().top }, "slow")
+	  $("html, body").animate({ scrollTop: $('#contact-and-pic').offset().top }, "slow");
 	  return false;
 	});
 
+	//Slow scrolls to the education section
+	education.click(function() {
+	  $("html, body").animate({ scrollTop: $('#education').offset().top }, "slow");
+	  return false;
+	});
+
+	//Slow scrolls to the experience section
+	experience.click(function() {
+	  $("html, body").animate({ scrollTop: $('#experience').offset().top }, "slow");
+	  return false;
+	});
+
+	//Slow scrolls to the qualifications section
 	qualifications.click(function() {
-	  $("html, body").animate({ scrollTop: $('#qualifications').offset().top }, "slow")
-	  //return false;
+	  $("html, body").animate({ scrollTop: $('#qualifications').offset().top }, "slow");
+	  return false;
 	});
 });
